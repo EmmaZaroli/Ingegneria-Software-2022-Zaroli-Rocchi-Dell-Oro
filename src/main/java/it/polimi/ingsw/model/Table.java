019196bbc0fe90ce93127bd58d1ed8b1d474579e
EmmaZaroli <100956315+EmmaZaroli@users.noreply.github.com>
2022-03-12 12:52:28 +0100

    add Table draft

---
+++ src/main/java/it/polimi/ingsw/model/Table.java
@@ -0,0 +1,30 @@
+package it.polimi.ingsw.model;
+
+import it.polimi.ingsw.model.enums.*;
+import java.util.*;
+
+
+public class Table {
+    private Set<PawnColor> professors;
+    private List<IslandCard> islandCards;
+    private final Bag bag;
+    private final List<CloudTile> cloudTiles;
+
+    public Table(PlayerCountIcon playerCountIcon) {
+        professors = new HashSet<>();
+        professors.addAll(Arrays.asList(PawnColor.values()));
+        islandCards=new ArrayList<>();
+        for(int i=0;i<12;i++){
+            this.islandCards.add(new IslandCard());
+        }
+        this.bag = new Bag();
+        cloudTiles=new ArrayList<>();
+        this.cloudTiles.add(new CloudTile(playerCountIcon));
+        this.cloudTiles.add(new CloudTile(playerCountIcon));
+        if(playerCountIcon.equals(PlayerCountIcon.THREE)){
+            this.cloudTiles.add(new CloudTile(playerCountIcon));
+        }
+    }
+
+
+}
