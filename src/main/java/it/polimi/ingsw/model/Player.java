beb7c025f32061f075c3cf39de3d33825c39ef72
riccardodelloro <riccardo.delloro@mail.polimi.it>
2022-03-16 11:29:39 +0100

    Update Player

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -1,16 +1,35 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.model.enums.Tower;
 import it.polimi.ingsw.model.enums.Wizzard;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class Player {
     private final String nickname;
     private final Wizzard wizzard;
     private AssistantCard discardPileHead;
+    private boolean isPlayerTurn;
+    private final SchoolBoard schoolBoard;
+    private final List<AssistantCard> assistantDeck;
 
-    public Player(String nickname, Wizzard wizzard) {
+    public Player(String nickname, Wizzard wizzard, Tower tower) {
+        this(nickname, wizzard, tower, false);
+    }
+
+    public Player(String nickname, Wizzard wizzard, Tower tower, boolean isPlayerTurn) {
         this.nickname = nickname;
         this.wizzard = wizzard;
         this.discardPileHead = null;
+        this.isPlayerTurn = isPlayerTurn;
+        this.schoolBoard = new SchoolBoard(8, tower);
+        //TODO parametrizzare numero di torri
+        this.assistantDeck = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            //TODO parametrizzare il numero di carte
+            this.assistantDeck.add(new AssistantCard(i + 1, ((int) (i / 2)) + 1));
+        }
     }
 
     public String getNickname() {
@@ -19,6 +38,22 @@ public class Player {
 
     public Wizzard getWizzard() {
         return this.wizzard;
+    }
+
+    public boolean isPlayerTurn() {
+        return isPlayerTurn;
+    }
+
+    public void setPlayerTurn(boolean playerTurn) {
+        isPlayerTurn = playerTurn;
+    }
+
+    public void togglePlayerTurn() {
+        isPlayerTurn = !isPlayerTurn;
+    }
+
+    public SchoolBoard getSchoolBoard() {
+        return schoolBoard;
     }
 
     protected void playAssistant(AssistantCard a) {
