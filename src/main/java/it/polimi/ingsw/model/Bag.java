c36823a8349632296feaff9883241ad7b6156452
EmmaZaroli <100956315+EmmaZaroli@users.noreply.github.com>
2022-03-16 13:28:58 +0100

    Update Table and IslandCard

--- src/main/java/it/polimi/ingsw/model/Bag.java
+++ src/main/java/it/polimi/ingsw/model/Bag.java
@@ -1,40 +1,37 @@
 package it.polimi.ingsw.model;
 
-import it.polimi.ingsw.model.enums.*;
+import it.polimi.ingsw.model.enums.PawnColor;
 import it.polimi.ingsw.utils.RandomHelper;
 
 import java.util.EnumMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class Bag {
+    private static final int PAWNS_PER_COLOR = 24;
+
     private final Map<PawnColor, Integer> students;
     private int pawnCount;
 
-    public Bag() {
+    protected Bag() {
         this.students = new EnumMap<>(PawnColor.class);
         for (PawnColor pc : PawnColor.values()) {
-            students.put(pc, 0);
+            students.put(pc, PAWNS_PER_COLOR);
+            this.pawnCount += PAWNS_PER_COLOR;
         }
-
-        this.pawnCount = getPawnCount();
-    }
-
-    protected void addStudents(PawnColor student, int size) {
-        Integer studentsCount = this.students.get(student);
-        studentsCount += size;
-        this.students.replace(student, studentsCount);
     }
 
     protected PawnColor drawStudent() {
         RandomHelper randomHelper = RandomHelper.getInstance();
-
-        int random = randomHelper.getInt(pawnCount);
+        //randomHelper.getInt(n) returns an integer in [0, n), so we need to add 1
+        int random = randomHelper.getInt(pawnCount) + 1;
         int pos = 0;
 
         for (Map.Entry<PawnColor, Integer> e : students.entrySet()) {
             pos += e.getValue();
-            if (pos >= random) {
-                pawnCount--;
+            if (random <= pos) {
+                removePawn(e.getKey());
                 return e.getKey();
             }
         }
@@ -42,10 +39,21 @@ public class Bag {
         return PawnColor.RED;
     }
 
-    private int getPawnCount() {
-        return students
-                .entrySet()
-                .stream()
-                .reduce(0, (tmp, element) -> tmp + element.getValue(), Integer::sum);
+    protected List<PawnColor> drawStudents(int n) {
+        List<PawnColor> drawnStudents = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            drawnStudents.add(drawStudent());
+        }
+
+        return drawnStudents;
+    }
+
+    //Removes a pawn from the bag
+    private void removePawn(PawnColor c) {
+        Integer pawnColorCount = this.students.get(c);
+        pawnColorCount = pawnColorCount + 1;
+        this.students.replace(c, pawnColorCount);
+        this.pawnCount--;
     }
 }
--- src/main/java/it/polimi/ingsw/model/IslandCard.java
+++ src/main/java/it/polimi/ingsw/model/IslandCard.java
@@ -7,25 +7,44 @@ import it.polimi.ingsw.model.enums.*;
 public class IslandCard {
     private boolean hasMotherNature;
     private final List<PawnColor> students;
-    /*
-    private List<Tower> towers;
+    private Tower tower;
     private int size;
-     */
+
     public IslandCard() {
         this.students = new ArrayList<>();
-         /*
-        towers=new ArrayList<>();
+        tower = Tower.NONE;
         size=1;
-        */
+        hasMotherNature=false;
     }
 
-    public boolean isHasMotherNature() {
-        return hasMotherNature;
+    public void setMotherNatureHere(boolean b) {
+        hasMotherNature=b;
     }
 
     public void movePawnOnIsland(PawnColor student){
         students.add(student);
     }
 
-    //public int countInfluence(){}
+    public int countInfluence(List<PawnColor> professors, Tower towerColor) {
+        int influence = 0;
+        for (PawnColor p : students) {
+            if (professors.contains(p)) {
+                influence++;
+            }
+        }
+        if (towerColor.equals(tower)) {
+            influence++;
+        }
+        return influence;
+    }
+
+    public int getSize() {
+        return size;
+    }
+    public Tower getTower() {
+        return tower;
+    }
+    public void setTower(Tower tower) {
+        this.tower=tower;
+    }
 }
