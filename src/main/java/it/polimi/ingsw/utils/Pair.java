79fc9e5bb8980a67456ba238f137c0e56e6d6cd7
EmmaZaroli <100956315+EmmaZaroli@users.noreply.github.com>
2022-03-16 13:28:58 +0100

    Update Table and IslandCard

---
+++ src/main/java/it/polimi/ingsw/utils/Pair.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.utils;
+
+import it.polimi.ingsw.model.enums.Tower;
+
+public class Pair {
+    Tower tower;
+    int size;
+
+    public Pair(Tower tower, int size) {
+        this.tower = tower;
+        this.size = size;
+    }
+
+}
