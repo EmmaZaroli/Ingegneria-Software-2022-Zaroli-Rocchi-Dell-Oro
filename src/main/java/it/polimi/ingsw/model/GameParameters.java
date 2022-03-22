084624ffd4818d14bdbe2872396803733b239ce0
riccardodelloro <riccardo.delloro@mail.polimi.it>
2022-03-22 16:42:14 +0100

    Add GameParameters

---
+++ src/main/java/it/polimi/ingsw/model/GameParameters.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.model;
+
+import it.polimi.ingsw.model.enums.PawnColor;
+
+public class GameParameters {
+    private int initialTowersCount;
+    private int initialStudentsCount;
+    private int studentsToDraw;
+    private int studentsToMove;
+
+    private boolean takeProfessorEvenIfSameStudents;
+    private int motherNatureExtraMovements;
+    private boolean towersCountInInfluence;
+    private int extraInfluence;
+    private PawnColor colorWithNoInfluence;
+}
