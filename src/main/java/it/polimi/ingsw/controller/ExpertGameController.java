f0436ede950d5fe4cd2a9af0cc04a90dcf5a5065
EmmaZaroli <100956315+EmmaZaroli@users.noreply.github.com>
2022-03-24 18:11:09 +0100

    SonarLint small correction

---
+++ src/main/java/it/polimi/ingsw/controller/ExpertGameController.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.controller;
+
+import it.polimi.ingsw.model.Player;
+
+public class ExpertGameController extends GameController {
+    public ExpertGameController(Player[] players) {
+        super(players);
+
+    }
+
+    public void canActivateCharacterAbility(Character character) {
+
+    }
+
+    public void activateCharacterAbility(Character character) {
+
+    }
+}
