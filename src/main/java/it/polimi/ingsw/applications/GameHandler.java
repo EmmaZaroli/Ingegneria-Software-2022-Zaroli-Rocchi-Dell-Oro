b0ed48e1f0a154853be1355005cf0d6d290ef45a
riccardodelloro <riccardo.delloro@mail.polimi.it>
2022-04-16 16:19:09 +0200

    Update GameHandler

--- src/main/java/it/polimi/ingsw/applications/GameHandler.java
+++ src/main/java/it/polimi/ingsw/applications/GameHandler.java
@@ -2,7 +2,7 @@ package it.polimi.ingsw.applications;
 
 import it.polimi.ingsw.controller.GameController;
 
-public class GameHandler implements Runnable{
+public class GameHandler extends Thread{
     GameController gameController;
 
     public GameHandler(GameController gameController) {
