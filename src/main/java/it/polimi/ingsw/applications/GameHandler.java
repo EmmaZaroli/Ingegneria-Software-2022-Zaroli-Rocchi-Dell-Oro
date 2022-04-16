19c4ffa882d0849c346dc47850b7b8e67aec3a83
riccardodelloro <riccardo.delloro@mail.polimi.it>
2022-04-16 16:17:51 +0200

    Create GameHandler

--- src/main/java/it/polimi/ingsw/applications/GameHandler.java
+++ src/main/java/it/polimi/ingsw/applications/GameHandler.java
@@ -1,51 +1,16 @@
 package it.polimi.ingsw.applications;
 
-import it.polimi.ingsw.applications.enums.NicknameStatus;
 import it.polimi.ingsw.controller.GameController;
-import it.polimi.ingsw.network.Endpoint;
 
-public class GameHandler extends Thread{
+public class GameHandler implements Runnable{
     GameController gameController;
-    User[] users;
 
     public GameHandler(GameController gameController) {
         this.gameController = gameController;
-        //TODO get users
-        //TODO get view
     }
 
     @Override
     public void run() {
 
-    }
-
-    public NicknameStatus checkNicknameStatus(String nickname){
-        NicknameStatus status = NicknameStatus.FREE;
-        for(User user: users){
-            if(user.getNickname().equals(nickname)) {
-                if (user.isOnline())
-                    status = NicknameStatus.FROM_CONNECTED_PLAYER;
-                else
-                    status = NicknameStatus.FROM_DISCONNECTED_PLAYER;
-            }
-        }
-        return status;
-    }
-
-    public boolean containsUser(String nickname){
-        for(User user: users){
-            if(user.getNickname().equals(nickname))
-                return true;
-        }
-        return false;
-    }
-
-    public void reconnectPlayer(String nickname, Endpoint endpoint){
-        for(User user: users){
-            if (user.getNickname().equals(nickname)){
-                user.setEndpoint(endpoint);
-                user.setOnline();
-            }
-        }
     }
 }
