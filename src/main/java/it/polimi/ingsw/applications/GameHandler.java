de5cb5b9d20e779c50174e157e193e45fa66df1d
riccardodelloro <riccardo.delloro@mail.polimi.it>
2022-04-17 15:38:51 +0200

    Implement Server

--- src/main/java/it/polimi/ingsw/applications/GameHandler.java
+++ src/main/java/it/polimi/ingsw/applications/GameHandler.java
@@ -1,16 +1,33 @@
 package it.polimi.ingsw.applications;
 
+import it.polimi.ingsw.applications.enums.NicknameStatus;
 import it.polimi.ingsw.controller.GameController;
 
 public class GameHandler extends Thread{
     GameController gameController;
+    User[] users;
 
     public GameHandler(GameController gameController) {
         this.gameController = gameController;
+        //TODO get users
+        //TODO get view
     }
 
     @Override
     public void run() {
 
+    }
+
+    public NicknameStatus checkNicknameStatus(String nickname){
+        NicknameStatus status = NicknameStatus.FREE;
+        for(User user: users){
+            if(user.getNickname().equals(nickname)) {
+                if (user.isOnline())
+                    status = NicknameStatus.FROM_CONNECTED_PLAYER;
+                else
+                    status = NicknameStatus.FROM_DISCONNECTED_PLAYER;
+            }
+        }
+        return status;
     }
 }
