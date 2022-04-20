fd9b1849c1a776446587c4c2ded4b26e01d712a7
riccardodelloro <riccardo.delloro@mail.polimi.it>
2022-04-20 12:55:52 +0200

    Implement reconnectPlayer

--- src/main/java/it/polimi/ingsw/applications/GameHandler.java
+++ src/main/java/it/polimi/ingsw/applications/GameHandler.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.applications;
 
 import it.polimi.ingsw.applications.enums.NicknameStatus;
 import it.polimi.ingsw.controller.GameController;
+import it.polimi.ingsw.network.Endpoint;
 
 public class GameHandler extends Thread{
     GameController gameController;
@@ -29,5 +30,22 @@ public class GameHandler extends Thread{
             }
         }
         return status;
+    }
+
+    public boolean containsUser(String nickname){
+        for(User user: users){
+            if(user.getNickname().equals(nickname))
+                return true;
+        }
+        return false;
+    }
+
+    public void reconnectPlayer(String nickname, Endpoint endpoint){
+        for(User user: users){
+            if (user.getNickname().equals(nickname)){
+                user.setEndpoint(endpoint);
+                user.setOnline();
+            }
+        }
     }
 }
