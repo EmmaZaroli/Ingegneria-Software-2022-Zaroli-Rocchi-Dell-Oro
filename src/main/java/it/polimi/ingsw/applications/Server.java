7244684c37a1144434c6010a3159b8846ddce96b
Marco Rocchi <marco.rocchi.2000.24@gmail.com>
2022-04-20 20:46:55 +0200

    Solve some todos

--- src/main/java/it/polimi/ingsw/applications/Server.java
+++ src/main/java/it/polimi/ingsw/applications/Server.java
@@ -38,10 +38,8 @@ public class Server {
     private final List<GameHandler> expert3PlayersRunningGames = new LinkedList<>();
 
     public static void main(String[] args) {
-        //TODO start thread
-
-        if(args.length < 1 || !args[0].equals("--port") || !isNumeric(args[1])) {
-            logger.log(Level.SEVERE, MessagesHelper.noPort);
+        if (args.length < 1 || !args[0].equals("--port") || !isNumeric(args[1])) {
+            logger.log(Level.SEVERE, MessagesHelper.NO_PORT);
         } else {
             int port = Integer.parseInt(args[1]);
 
@@ -58,7 +56,7 @@ public class Server {
         return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
     }
 
-    public Server(int port){
+    public Server(int port) {
         this.port = port;
     }
 
@@ -66,18 +64,18 @@ public class Server {
         //It creates threads when necessary, otherwise it re-uses existing one when possible
         ExecutorService executor = Executors.newCachedThreadPool();
         ServerSocket serverSocket;
-        try{
+        try {
             serverSocket = new ServerSocket(port);
-        }catch (IOException e){
-            logger.log(Level.SEVERE, MessagesHelper.portNotAvailable);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, MessagesHelper.PORT_NOT_AVAILABLE);
             return;
         }
-        logger.log(Level.INFO, MessagesHelper.serverStarted);
-        while (true){
-            try{
+        logger.log(Level.INFO, MessagesHelper.SERVER_STARTED);
+        while (true) {
+            try {
                 Socket socket = serverSocket.accept();
                 executor.submit(new UserHandler(socket, this));
-            }catch(IOException e){
+            } catch (IOException e) {
                 break; //In case the serverSocket gets closed
             }
         }
@@ -92,14 +90,13 @@ public class Server {
     }
 
     public void enqueueUser(User user, GameMode selectedGameMode, PlayersNumber selectedPlayersNumber) throws InvalidPlayerNumberException {
-        if (selectedGameMode == GameMode.NORMAL_MODE){
-            if(selectedPlayersNumber == PlayersNumber.TWO)
+        if (selectedGameMode == GameMode.NORMAL_MODE) {
+            if (selectedPlayersNumber == PlayersNumber.TWO)
                 enqueueNormal2Players(user);
             else
                 enqueueNormal3Players(user);
-        }
-        else{
-            if(selectedPlayersNumber == PlayersNumber.TWO)
+        } else {
+            if (selectedPlayersNumber == PlayersNumber.TWO)
                 enqueueExpert2Players(user);
             else
                 enqueueExpert3Players(user);
@@ -107,11 +104,11 @@ public class Server {
     }
 
     private void enqueueNormal2Players(User user) throws InvalidPlayerNumberException {
-        synchronized (normal2PlayersBuilder){
-            synchronized (normal2PlayersRunningGames){
+        synchronized (normal2PlayersBuilder) {
+            synchronized (normal2PlayersRunningGames) {
                 normal2PlayersBuilder.player(user.getNickname());
 
-                if(normal2PlayersBuilder.isGameFull()){
+                if (normal2PlayersBuilder.isGameFull()) {
                     //TODO have to pass virtualview and socket
                     GameController gameController = normal2PlayersBuilder.build();
                     GameHandler gameHandler = new GameHandler(gameController);
@@ -123,54 +120,55 @@ public class Server {
         }
     }
 
-    private void enqueueNormal3Players(User user){
+    private void enqueueNormal3Players(User user) {
         //TODO copy and modify enqueueNormal2Players
     }
 
-    private void enqueueExpert2Players(User user){
+    private void enqueueExpert2Players(User user) {
         //TODO copy and modify enqueueNormal2Players
     }
 
-    private void enqueueExpert3Players(User user){
+    private void enqueueExpert3Players(User user) {
         //TODO copy and modify enqueueNormal2Players
     }
 
-    public NicknameStatus checkNicknameStatus(String nickname){
+    public NicknameStatus checkNicknameStatus(String nickname) {
         NicknameStatus status = NicknameStatus.FREE;
 
-        for(GameHandler gameHandler: normal2PlayersRunningGames){
+        for (GameHandler gameHandler : normal2PlayersRunningGames) {
             status = gameHandler.checkNicknameStatus(nickname);
-            if(status != NicknameStatus.FREE)
+            if (status != NicknameStatus.FREE)
                 break;
         }
 
         return status;
     }
 
-    private Optional<GameHandler> getGameByPlayer(String nickname){
-        for(GameHandler gameHandler: normal2PlayersRunningGames){
-            if(gameHandler.containsUser(nickname))
+    private Optional<GameHandler> getGameByPlayer(String nickname) {
+        for (GameHandler gameHandler : normal2PlayersRunningGames) {
+            if (gameHandler.containsUser(nickname))
                 return Optional.of(gameHandler);
         }
-        for(GameHandler gameHandler: normal3PlayersRunningGames){
-            if(gameHandler.containsUser(nickname))
+        for (GameHandler gameHandler : normal3PlayersRunningGames) {
+            if (gameHandler.containsUser(nickname))
                 return Optional.of(gameHandler);
         }
-        for(GameHandler gameHandler: expert2PlayersRunningGames){
-            if(gameHandler.containsUser(nickname))
+        for (GameHandler gameHandler : expert2PlayersRunningGames) {
+            if (gameHandler.containsUser(nickname))
                 return Optional.of(gameHandler);
         }
-        for(GameHandler gameHandler: expert3PlayersRunningGames){
-            if(gameHandler.containsUser(nickname))
+        for (GameHandler gameHandler : expert3PlayersRunningGames) {
+            if (gameHandler.containsUser(nickname))
                 return Optional.of(gameHandler);
         }
         return Optional.empty();
     }
 
-    public void reconnectPlayer(String nickname, Endpoint endpoint){
+    public void reconnectPlayer(String nickname, Endpoint endpoint) {
         Optional<GameHandler> gameHandler = getGameByPlayer(nickname);
         //TODO this may cause to throw an exception
-        gameHandler.get();
+        if (gameHandler.isPresent()) {
+            gameHandler.get();
+        }
     }
-
 }
