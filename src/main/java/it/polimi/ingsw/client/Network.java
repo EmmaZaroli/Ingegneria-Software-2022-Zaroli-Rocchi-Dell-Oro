a8b450c2adff7b4fffc85b1e6ec9ce378b69420f
Marco Rocchi <marco.rocchi.2000.24@gmail.com>
2022-05-03 15:39:04 +0200

    Refactor abstract view

--- src/main/java/it/polimi/ingsw/client/Network.java
+++ src/main/java/it/polimi/ingsw/client/Network.java
@@ -4,11 +4,8 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 public class Network implements PropertyChangeListener {
-
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         String propertyName = evt.getPropertyName();
     }
-    
-
 }
