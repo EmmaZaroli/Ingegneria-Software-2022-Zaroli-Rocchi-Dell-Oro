b8b4372f8810711ac71c8392f7672517f6319498
Marco Rocchi <marco.rocchi.2000.24@gmail.com>
2022-05-03 15:39:04 +0200

    Refactor abstract view

--- src/main/java/it/polimi/ingsw/applications/Client.java
+++ src/main/java/it/polimi/ingsw/applications/Client.java
@@ -1,7 +1,22 @@
 package it.polimi.ingsw.applications;
 
+import it.polimi.ingsw.client.View;
+
 public class Client {
+
+    private View view;
+
     public static void main(String[] args) {
-        Client client = new Client();
+        if (args.length == 0) {
+            //TODO start gui
+        } else {
+            if (args[0].equals("--cli")) {
+                //TODO start cli
+            }
+        }
+    }
+
+    public Client(View view) {
+        this.view = view;
     }
 }
