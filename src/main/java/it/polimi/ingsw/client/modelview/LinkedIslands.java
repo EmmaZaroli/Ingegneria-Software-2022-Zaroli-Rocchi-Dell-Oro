b9c885074b5cb9f03abce276a268a13681d13bcf
EmmaZaroli <100956315+EmmaZaroli@users.noreply.github.com>
2022-05-04 19:34:04 +0200

    adjust class LinkedIslands

--- src/main/java/it/polimi/ingsw/client/modelview/LinkedIslands.java
+++ src/main/java/it/polimi/ingsw/client/modelview/LinkedIslands.java
@@ -1,20 +1,36 @@
 package it.polimi.ingsw.client.modelview;
 
-import it.polimi.ingsw.model.CloudTile;
 import it.polimi.ingsw.model.IslandCard;
 
 import java.util.List;
 
 public class LinkedIslands {
 
-    private List<CloudTile> clouds;
+    private IslandCard island;
+    private List<IslandCard> linkedislands;
+    private boolean connected = false;
 
-    public void getLinkedIslands() {
-        //TODO
+    public IslandCard getMainIsland() {
+        return island;
     }
 
-    public void addLinkedIslands(IslandCard newIsland) {
-        //TODO
+    public void setMainIsland(IslandCard island) {
+        this.island = island;
     }
 
+    public List<IslandCard> getLinkedislands() {
+        return linkedislands;
+    }
+
+    public void setLinkedislands(List<IslandCard> linkedislands) {
+        this.linkedislands = linkedislands;
+    }
+
+    public boolean isConnected() {
+        return connected;
+    }
+
+    public void setConnected() {
+        this.connected = true;
+    }
 }
