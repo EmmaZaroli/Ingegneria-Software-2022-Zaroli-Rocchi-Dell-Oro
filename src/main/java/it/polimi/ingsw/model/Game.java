81b084c36c917f4c83a00d072a4fbaac36333b57
EmmaZaroli <100956315+EmmaZaroli@users.noreply.github.com>
2022-05-26 12:41:04 +0200

    update Observer/Observable

--- src/main/java/it/polimi/ingsw/model/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -144,10 +144,9 @@ public class Game extends Observable implements Serializable {
     }
 
     public void changePlayer(int playerIndex) {
-        System.out.println("changed player in game");
         setCurrentPlayer(playerIndex);
         setCurrentPlayerBoard(getCurrentPlayerSchoolBoard());
-        notify(this.players[currentPlayer]);
+        notifyModelObserver(this.players[currentPlayer]);
     }
 
     public void setError(Exception e) {
