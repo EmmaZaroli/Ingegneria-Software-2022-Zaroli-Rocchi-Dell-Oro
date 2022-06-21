834586e0c4f58ec6d76d43705f554a9f9e474601
riccardodelloro <riccardo.delloro@mail.polimi.it>
2022-06-21 15:03:18 +0200

    Update Bag

--- src/main/java/it/polimi/ingsw/model/Bag.java
+++ src/main/java/it/polimi/ingsw/model/Bag.java
@@ -25,17 +25,13 @@ public class Bag implements Serializable {
      */
     protected Bag() {
         this.students = new EnumMap<>(PawnColor.class);
-        /*
         for (PawnColor pc : PawnColor.values()) {
             if (pc != PawnColor.NONE) {
                 students.put(pc, PAWNS_PER_COLOR);
                 this.pawnCount += PAWNS_PER_COLOR;
             }
-        }*/
-        students.put(PawnColor.RED, PAWNS_PER_COLOR*5);
-        this.pawnCount += PAWNS_PER_COLOR*5;
+        }
     }
-
     /**
      * Draws a student and removes it from the bag
      *
