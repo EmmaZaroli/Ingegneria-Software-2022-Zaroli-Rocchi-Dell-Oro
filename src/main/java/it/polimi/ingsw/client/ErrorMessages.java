f5778d16ef4d32b8ff62af78b99c602e21dc003e
riccardodelloro <riccardo.delloro@mail.polimi.it>
2022-06-27 14:33:03 +0200

    Handle disconnection client side

--- src/main/java/it/polimi/ingsw/client/ErrorMessages.java
+++ src/main/java/it/polimi/ingsw/client/ErrorMessages.java
@@ -6,4 +6,5 @@ package it.polimi.ingsw.client;
 public class ErrorMessages {
     public static final String NO_NETWORK = "Error while trying to establish a connection with the server";
     public static final String PARAMETERS_ERROR = "The server is unable to start a game with the given parameters";
+    public static final String DISCONNECTION = "Connection whit server has ended";
 }
